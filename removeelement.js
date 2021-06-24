dd072fcdd01ba700fcb54f8b3f03f18e8d8da969
Arthur Baker <artbaker82@gmail.com>
2021-06-23 17:25:25 -0700

    remove element correct solutions

--- removeelement.js
+++ removeelement.js
@@ -5,7 +5,21 @@ const removeElement = (nums, val) => {
       return removeElement(nums, val);
     }
   }
+  return nums.length;
+};
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+/*
+const removeElement = (nums, val) => {
+  let pointer1 = 0;
+  for (let pointer2 = 0; pointer2 < nums.length; pointer2++) {
+    if (nums[pointer2] !== val) {
+      nums[pointer1] = nums[pointer2];
+      pointer1++;
+    }
+  }
   return nums;
 };
 
-console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+*/
