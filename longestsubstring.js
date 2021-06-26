5cce68ef92a5903f1c13dacad35f05ff780d5694
Arthur Baker <artbaker82@gmail.com>
2021-06-26 12:06:42 -0700

    longest substring solved

--- longestsubstring.js
+++ longestsubstring.js
@@ -1,3 +1,4 @@
+/*
 const lengthOfLongestSubstring = (s) => {
   //convert to array, store each value in temporary array, compare each new value to each value in temp array,
   //when duplicate is found, start over from index of duplicate
@@ -10,6 +11,8 @@ const lengthOfLongestSubstring = (s) => {
   let prevLength = 0;
   let length = 0;
   for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    console.log(longestString);
     //if longeststring arr does not include arr[i]
     //push arr[i] to longest string
     if (!longestString.includes(arr[i])) {
@@ -23,8 +26,13 @@ const lengthOfLongestSubstring = (s) => {
       if (length > prevLength) {
         prevLength = length;
         longestString = [];
-        if (arr[i] !== arr[i - 1]) {
-          i--;
+        if (arr[i] !== arr[i - 2]) {
+          console.log(
+            `${arr[i - 2]} has been pushed to the new empty arrray because ${arr[i]} !== ${
+              arr[i - 2]
+            }`
+          );
+          i -= 2;
           longestString.push(arr[i]);
           length = longestString.length;
         } else {
@@ -41,4 +49,58 @@ const lengthOfLongestSubstring = (s) => {
   return length === 0 ? longestString.length : prevLength;
   //length > prevLength ? length : prevLength;
 };
+console.log(lengthOfLongestSubstring("abcabcbb"));
+
+*/
+
+//reverse the logic
+
+const lengthOfLongestSubstring = (s) => {
+  //convert to array, store each value in temporary array, compare each new value to each value in temp array,
+  //when duplicate is found, start over from index of duplicate
+  let arr = s.split("");
+  //if string is only 1 item, return the length
+  if (arr.length === 1) {
+    return arr.length;
+  }
+  let longestString = [];
+  let currentLongest = 0;
+  let previousLongest = 0;
+  let record = [];
+  let duplicateFound;
+  for (let i = 0; i < arr.length; i++) {
+    //if longest string DOES include the current index
+
+    console.log("Current longest array:" + longestString);
+    //CHANGE THIS! LOOP BACKWARDS UNTIL DUPLICATE IS FOUND, CHANGE i to that value + 1
+    if (longestString.includes(arr[i])) {
+      previousLongest = currentLongest;
+      longestString = [];
+      duplicateFound = arr[i];
+
+      do {
+        i--;
+      } while (arr[i] !== duplicateFound);
+
+      longestString.push(arr[i + 1]);
+      i += 1;
+    } else {
+      //no duplicate was found
+      longestString.push(arr[i]);
+    }
+
+    currentLongest = longestString.length;
+
+    record.push(currentLongest, previousLongest);
+  }
+
+  if (record.length > 0) {
+    return record.reduce((a, b) => {
+      return a > b ? a : b;
+    });
+  } else {
+    return 0;
+  }
+};
+
 console.log(lengthOfLongestSubstring("abcabcbb"));
